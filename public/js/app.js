52524219e4f24ba237c3efe2f3a1f3aa72ac20c6
reidsm <einsacken@gmail.com>
2021-01-15 23:37:55 -0800

    added client

---
+++ public/js/app.js
@@ -0,0 +1,70 @@
+var apiKey = "47082344";
+var sessionId = "1_MX40NzA4MjM0NH5-MTYxMDY4ODEyODg4OX5ycDRnZ2tVU3hMSHMrWkVKR21EdytuTmt-fg";
+var token = "T1==cGFydG5lcl9pZD00NzA4MjM0NCZzaWc9MTUxM2UyNTZlMDkwNTgwZDIzZjk3YzkzZDk4YWM1ZTBhODJiMDdjMTpzZXNzaW9uX2lkPTFfTVg0ME56QTRNak0wTkg1LU1UWXhNRFk0T0RFeU9EZzRPWDV5Y0RSbloydFZVM2hNU0hNcldrVktSMjFFZHl0dVRtdC1mZyZjcmVhdGVfdGltZT0xNjEwNjg4MTYwJm5vbmNlPTAuNzE5MTM4OTQ1MDkzNzU4NCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjEzMjgwMTU4JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
+
+// (optional) add server code here
+
+var SERVER_BASE_URL = 'http://localhost:8080';
+    fetch(SERVER_BASE_URL + '/session').then(function(res) {
+      return res.json()
+    }).then(function(res) {
+      apiKey = res.apiKey;
+      sessionId = res.sessionId;
+      token = res.token;
+      initializeSession();
+    }).catch(handleError);
+
+initializeSession();
+
+// Handling all of our errors here by alerting them
+function handleError(error) {
+    if (error) {
+      alert(error.message);
+    }
+  }
+  /**
+   * Function definition for initializeSession(), creates session with our credentials
+   * and connects to the session. 
+   */
+  function initializeSession() {
+    var session = OT.initSession(apiKey, sessionId);
+  
+    /**
+     * Subscribe to a newly created stream. 
+     * @param event.stream is the stream object to which the client is subscribing
+     * @param set of properties that define the appearance of the subscriber view
+     * @param completion handler (optional) that is called when the subscribe() method
+     * succeeds or fails
+     */
+  
+    session.on('streamCreated', function(event) {
+        session.subscribe(event.stream, 'subscriber', {
+          insertMode: 'append',
+          width: '100%',
+          height: '100%'
+        }, handleError);
+      });
+
+    /**
+     * initPublisher initializes publisher object with OT.initPublisher.
+     * @param publisher the div being replaced by this dom element
+     * @param insertMode, width, height, are the properties of this publisher
+     * @param optional, not present here (handle error not defined), is the 
+     *  competion handler specification
+     */
+    var publisher = OT.initPublisher('publisher', {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
+    }, handleError);
+  
+    // Connect to the session
+    session.connect(token, function(error) {
+      // If the connection is successful, publish to the session
+      if (error) {
+        handleError(error);
+      } else {
+        session.publish(publisher, handleError);
+      }
+    });
+  }
